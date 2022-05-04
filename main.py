d161c64e110dc3264d6116b1646c2b91062a02c6
MGS <05soufiane@gmail.com>
2022-05-04 17:05:13 +0000

    Print results

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from auth.auth import SpotifyAuth
+from app.test_playlist import Playlist
 
 def	main():
 	client_id = input("Enter your Client ID: ")
 	client_secret = input("Enter your Client Secret: ")
 	spotify = SpotifyAuth(client_id, client_secret)
-	spotify.perform_auth()
+	access_token = spotify.get_access_token()
+	print(access_token)
+	playlists = Playlist(access_token)
+	print(playlists.get_user_playlists())
 
 if __name__ == "__main__":
 	main()
