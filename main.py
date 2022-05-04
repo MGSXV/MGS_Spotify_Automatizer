9044e5306199d78a4c4f18571e7d657657062203
MGS <05soufiane@gmail.com>
2022-05-04 02:06:35 +0000

    Testing phase

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from auth.auth import SpotifyAuth
+
 def	main():
-	print("Hello World")
+	client_id = input("Enter your Client ID: ")
+	client_secret = input("Enter your Client Secret: ")
+	spotify = SpotifyAuth(client_id, client_secret)
+	spotify.perform_auth()
 
 if __name__ == "__main__":
 	main()
